240738e69d6f5293ef14d78a5b9b594891e3ca3b
Mainman Slim <mike@thehillengroup.net>
2025-07-27 23:50:05 -0400

    Changed text in footer. v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center py-6">
       <p className="text-sm">
-        &copy; {new Date().getFullYear()} The Hillen Group. All rights are reserved, folks.
+        &copy; {new Date().getFullYear()} The Hillen Group. All rights reserved, folks.
       </p>
     </footer>
   );
