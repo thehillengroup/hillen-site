9fd79cdcec92ef1ac913f7229ddabd91df3d73e2
Mainman Slim <mike@thehillengroup.net>
2025-08-30 09:11:04 -0400

    Fixed breadcrumbs.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -33,7 +33,7 @@ export default function Services() {
         <div className="max-w-7xl mx-auto px-4">
           <Breadcrumbs
             items={[
-              { label: 'Home', to: '/home' },
+              { label: 'Home', href: '/home' },
               { label: 'Services' },
             ]}
           />
